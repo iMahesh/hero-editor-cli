2bb7dfb759ce773f73c1b704cb8253e6e65a7861
Mahesh <reddy.mahesh001@gmail.com>
2017-04-30 19:11:18 -0500

    Service and Dependency Injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,25 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero'
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-]
-
+import { HeroService } from './hero.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]
 })
 
 export class AppComponent {
-
   selectedHero:Hero;
   title: string = 'Tour of Heroes';
-  heroes=HEROES;
+  heroes:Hero[];
+  constructor(private _heroService:HeroService){}
+   getHeroes(): void {
+    this._heroService.getHeroes()
+    .then(heroes => this.heroes = heroes);
+  }
+  ngOnInit():void{
+    this.getHeroes();
+  }
   onSelect(hero:Hero):void{
     this.selectedHero=hero;
   }
